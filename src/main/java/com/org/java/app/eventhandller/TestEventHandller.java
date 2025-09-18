af03d61b648a0cda2b9d0c5e4d84c5873cece2a4
javatechivkp <sreenivasarao.javasoft9@gmail.com>
2025-09-18 18:26:52 +0530

    first commit

---
+++ src/main/java/com/org/java/app/eventhandller/TestEventHandller.java
@@ -0,0 +1,18 @@
+package com.org.java.app.eventhandller;
+
+import org.springframework.context.event.EventListener;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Component;
+import com.org.java.app.events.EmployeeEvent;
+
+@Component
+public class TestEventHandller {
+
+	@EventListener
+	@Async
+	public void cleanAndAssign(EmployeeEvent event) {
+		System.out.println(
+				"Employee Service: test service 2 " + event.getEmpName() + " : " + Thread.currentThread().getName());
+	}
+
+}
---
+++ src/test/java/com/org/java/app/JavaapplicationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.org.java.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaapplicationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
